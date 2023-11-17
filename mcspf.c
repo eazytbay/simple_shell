3add0a25558545dbcd8cbe404d628756fbb71712
eazytbay <eazytbay>
2023-11-17 05:00:13 -0800

    mcspf.c

--- mcspf.c
+++ mcspf.c
@@ -1,15 +1,15 @@
 #include "simple_shell.h"
 /**
  * unique_shell_cmd - A function that checks the executability of a file
- * @info: The information 
- * @path: the absolute or relative path to the file being checked
+ * @stinf: The information 
+ * @abs: the absolute or relative path to the file being checked
  * Return: 1 if the file is executable and 0 if not.
  */
-int unique_shell_cmd(unique_info_t *info, char *path)
+int unique_shell_cmd(unique_info_t *stinf, char *abs)
 {
 struct stat st;
-(void)info;
-if (!path || stat(path, &st))
+(void)stinf;
+if (!abs || stat(path, &st))
 return (0);
 if (st.st_mode & S_IFREG)
 {
@@ -19,19 +19,19 @@ return (0);
 }
 /**
  * unique_shell_char_dup - A function that can duplicates characters
- * @pathstr: the PATH string
+ * @strdir: the PATH string
  * @begin: The beginning
  * @end: the very end
  * Return: pointer to a new buffer that has been specified
  */
-char *unique_shell_char_dup(char *pathstr, int begin, int end)
+char *unique_shell_char_dup(char *strdir, int begin, int end)
 {
 static char buffer[1024];
 int x = 0, y = 0;
 while (x >= begin && x < end)
 {
-if (pathstr[x] != ':')
-buffer[y++] = pathstr[x];
+if (strdir[x] != ':')
+buffer[y++] = strdir[x];
 x++;
 }
 buffer[y] = 0;
@@ -39,10 +39,10 @@ return (buffer);
 }
 /**
  * unique_shell_path_loc -  A function that locates the cmd in the string PATH
- * @info: The information
+ * @st: The information
  * @strpat: the expeted PATH of the string
- * @cmd: the cmd to located
- * Return: full path of cmd when its found or return  NULL if not
+ * @cmd: the cmd to be located
+ * Return: cmd full path
  */
 char *unique_shell_path_loc(unique_info_t *info, char *strpat, char *cmd)
 {
@@ -50,9 +50,9 @@ int x = 0, pp = 0;
 char *path;
 if (!strpat)
 return (NULL);
-if ((strlen(cmd) > 2) && strncmp(cmd, "./", 2) == 0)
+if ((custom_gen_strlen(cmd) > 2) && custom_gen_strncmp(cmd, "./", 2) == 0)
 {
-if (unique_shell_cmd(info, cmd))
+if (unique_shell_cmd(st, cmd))
 return (cmd);
 }
 for (; ; x++)
@@ -61,13 +61,13 @@ if (!strpat[x] || strpat[x] == ':')
 {
 path = unique_shell_char_dup(strpat, pp, x);
 if (!*path)
-strcat(path, cmd);
+custom_gen_strcat(path, cmd);
 else
 {
-strcat(path, "/");
-strcat(path, cmd);
+custom_gen_strcat(path, "/");
+custom_gen_strcat(path, cmd);
 }
-if (unique_shell_cmd(info, path))
+if (unique_shell_cmd(st, path))
 return (path);
 if (!strpat[x])
 break;
